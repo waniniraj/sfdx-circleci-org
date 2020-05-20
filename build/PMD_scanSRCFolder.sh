77838c92334f9de6253af84b68f34d706631eb43
Niraj Wani <nirajw@hexaware.com>
2020-05-20 13:42:24 +0530

    PMD folder scan

--- build/PMD_scanSRCFolder.sh
+++ build/PMD_scanSRCFolder.sh
@@ -2,6 +2,6 @@
 
 alias pmd="$HOME/pmd-bin-6.2.0/bin/run.sh pmd"
 echo 'Scanning src folder with custom Apex ruleset'
-pmd -d src -R build/PMD_apexBestPractices.xml -f summaryhtml -r build/PMD_results.html
+pmd -d force-app -R build/PMD_apexBestPractices.xml -f summaryhtml -r build/PMD_results.html
 echo 'Copying scan results to Artifacts folder'
 cp build/PMD_results.html "$CIRCLE_ARTIFACTS/PMD_results.html"
